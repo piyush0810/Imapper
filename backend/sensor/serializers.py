aa0bdcddf04b9c844ab780f10b7f2529c80bb79d
piyush0810 <piyushkumar0810@gmail.com>
2021-01-23 18:39:17 +0530

    added changes

---
+++ backend/sensor/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Sensor,Value
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sensor
+        fields = ('sensor_id','sensor_name','unit','dimensions','value')
+
+class sensSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sensor
+        fields = ('sensor_id','sensor_name','unit','dimensions')
+
+class ValueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Value
+        fields = '__all__'
